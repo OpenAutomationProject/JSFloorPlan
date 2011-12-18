8ddfd2fcf1b54f16192a44060d6bd5ca4dd8d010
mayerch <mayerch@b3ce15a5-05c3-4bc8-b6eb-98bdc3a999cb>
2011-12-18 14:39:40 +0000

    Set up files to start conversion to a clean library and document in at the same time

--- JSFloorPlan/trunk/src/jsfloorplan_example_helper.js
+++ JSFloorPlan/trunk/src/jsfloorplan_example_helper.js
@@ -1,28 +1,25 @@
-/*************************************************************************** 
- *                                                                         * 
- *  JavaScript FloorPlan 3D - helper functions for the example             * 
- *                                                                         * 
- *   Copyright (C) 2009 by Christian Mayer                                 *
- *   jsfloorplan (at) ChristianMayer.de                                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- *                                                                         * 
- * @module JSFloorPlan3D                                                   *
- * @submodule JSFloorPlan3D_example                                        *
- ***************************************************************************/
+//
+//  JavaScript FloorPlan 3D.
+//
+//   Copyright (C) 2009, 2010 by Christian Mayer
+//   jsfloorplan (at) ChristianMayer.de
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+//////////////////////////////////////////////////////////////////////////////
 
 
 function three_init()
@@ -168,8 +165,7 @@ scene.add( sunLightViewLine );
 /**
  * Provides requestAnimationFrame in a cross browser way.
  * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
- * @class window
- * @method requestAnimationFrame
+ * @class requestAnimationFrame
  */
 
 if ( !window.requestAnimationFrame ) {
